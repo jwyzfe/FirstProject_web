050743a1d0eeb447f73ea4a1c835bb043d98c2bd
Huey <demonic0319@gmail.com>
2024-11-27 03:36:17 +0000

    feat : main template update

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from app.routers import setup_routers
+from fastapi.templating import Jinja2Templates
 from app.database.database import init_db
 
 app = FastAPI()
@@ -21,9 +22,26 @@ app.add_middleware(
 async def read_root():
     return {"Hello": "World"}
 
+templates = Jinja2Templates(directory="app/templates/")
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(request:Request, item_id: int, q: str = None):
+    # results = {"item_id": item_id, "q": q}
+    first = item_id + 1000
+    results = f'''
+    '''
+    contexts = {'request':request, 'first_key':first, 'item_id_key':item_id}
+    return templates.TemplateResponse(name='items.html', context=contexts) # 여기에 html 주면 됨
+
+@app.get("/return_name/{name}")
+async def read_name(request:Request, name: str, q: str = None):
+    # results = {"item_id": item_id, "q": q}
+    # first = item_id + 1000
+    results = f'''
+    '''
+    contexts = {'request':request, 'print_name':name}
+    return templates.TemplateResponse(name='return_name.html', context=contexts) # 여기에 html 주면 됨
 
 from fastapi.staticfiles import StaticFiles
 # url 경로, 자원 물리 경로, 프로그램밍 측면
